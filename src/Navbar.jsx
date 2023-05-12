824b3fef9ee64a9763a6f10798a538e63ba66932
Jawuah1 <jessekofia@hotmail.com>
2023-05-12 19:45:40 -0400

    Intial commit

---
+++ src/Navbar.jsx
@@ -0,0 +1,11 @@
+
+
+export default function Navbar(){
+    return (
+    <nav>
+        
+        <img src="/images/airbnb-logo.png" />
+        <button className="air--button">Airbnb your home</button>
+    </nav>
+    )
+}
